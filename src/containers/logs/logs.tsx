f617579e3628769ae4068eebac4cc877beef7f6f
Ranielli Montagna <raniellimontagna@hotmail.com>
2024-09-11 18:32:18 -0300

    fix: remove logs from gitignore

---
+++ src/containers/logs/logs.tsx
@@ -0,0 +1,12 @@
+import { Header } from '@/components'
+
+export function Logs() {
+  return (
+    <div>
+      <Header
+        title="Registros"
+        description="Veja todos os registros de atividades realizadas na aplicação, para manter o controle de tudo."
+      />
+    </div>
+  )
+}
