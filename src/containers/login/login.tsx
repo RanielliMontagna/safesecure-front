db4627486601c06fa2dec8c1ad00caddc47bdeca
Ranielli Montagna <raniellimontagna@hotmail.com>
2024-09-11 13:18:58 -0300

    refactor: :recycle: optimize login screen responsiveness

--- src/containers/login/login.tsx
+++ src/containers/login/login.tsx
@@ -3,8 +3,7 @@ import { LoginForm } from './loginForm/loginForm'
 export function Login() {
   return (
     <>
-      <div className="md:hidden"></div>
-      <div className="container relative h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
@@ -20,8 +19,8 @@ export function Login() {
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div>
+          <div className="mx-auto flex w-full flex-col space-y-6 max-w-[350px] p-8">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
                 Entre com sua conta
